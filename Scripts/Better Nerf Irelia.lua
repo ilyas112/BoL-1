b06783d1cbd9d712d58e908c99a8038141ba3244
si7ziTV <m.a.schultheis.95@gmail.com>
2014-05-17 21:49:42 +0200

    Update Better Nerf Irelia.lua

--- Scripts/Better Nerf Irelia.lua
+++ Scripts/Better Nerf Irelia.lua
@@ -7,14 +7,15 @@ if myHero.charName ~= "Irelia" then return end
 local UPDATE_FILE_PATH = SCRIPT_PATH.."Better Nerf Irelia.lua"
 local UPDATE_NAME = "Better Nerf Irelia"
 local UPDATE_HOST = "raw.github.com"
-local UPDATE_PATH = "/si7ziTV/BoL/master/Scripts/Better%20Nerf%20Irelia.lua?chunk=..math.random(1, 1000)"
+local UPDATE_PATH = "/si7ziTV/BoL/master/Scripts/Better%20Nerf%20Irelia.lua?chunk="..math.random(1, 1000)
+local UPDATE_FILE_PATH = SCRIPT_PATH.."Better Nerf Irelia.lua"
 local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
  
 function AutoupdaterMsg(msg) print("<font color=\"#73DCFF\">["..IsLoaded.."]:</font> <font color=\"#FFDFBF\">"..msg..".</font>") end
 if IRELIAAUTOUPDATE then
     local ServerData = GetWebResult(UPDATE_HOST, UPDATE_PATH)
     if ServerData then
-        local ServerVersion = string.match(ServerData, "IreliaVersion = \"%d+.%d+\"")
+        local ServerVersion = string.match(ServerData, "local IreliaVersion = \"%d+.%d+\"")
         ServerVersion = string.match(ServerVersion and ServerVersion or "", "%d+.%d+")
         if ServerVersion then
             ServerVersion = tonumber(ServerVersion)
