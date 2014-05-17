abd8f90617c0ac1ab3b2854b09a6ebeaa04b95fd
si7ziTV <m.a.schultheis.95@gmail.com>
2014-05-17 21:25:47 +0200

    Update Better Nerf Irelia.lua

--- Scripts/Better Nerf Irelia.lua
+++ Scripts/Better Nerf Irelia.lua
@@ -1,9 +1,41 @@
-
------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------
-
+local IreliaVersion = "1.05"
+local IRELIAAUTOUPDATE = true
+local IreliaAuthor = "si7ziTV"
+local IsLoaded = "Better Nerf Irelia"
 if myHero.charName ~= "Irelia" then return end
+ 
+local UPDATE_FILE_PATH = SCRIPT_PATH.."Better Nerf Irelia.lua"
+local UPDATE_NAME = "Better Nerf Irelia"
+local UPDATE_HOST = "raw.github.com"
+local UPDATE_PATH = "https://github.com/si7ziTV/BoL/blob/master/Scripts/Better%20Nerf%20Irelia.lua?chunk="..math.random(1, 1000)
+local UPDATE_FILE_PATH = SCRIPT_PATH.."Better Nerf Irelia.lua"
+local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
+ 
+function AutoupdaterMsg(msg) print("<font color=\"#73DCFF\">["..IsLoaded.."]:</font> <font color=\"#FFDFBF\">"..msg..".</font>") end
+if IRELIAAUTOUPDATE then
+    local ServerData = GetWebResult(UPDATE_HOST, UPDATE_PATH)
+    if ServerData then
+        local ServerVersion = string.match(ServerData, "IreliaVersion = \"%d+.%d+\"")
+        ServerVersion = string.match(ServerVersion and ServerVersion or "", "%d+.%d+")
+        if ServerVersion then
+            ServerVersion = tonumber(ServerVersion)
+            if tonumber(IreliaVersion) < ServerVersion then
+                AutoupdaterMsg("A new version is available: ["..ServerVersion.."]")
+                AutoupdaterMsg("The script is updating... please don't press [F9]!")
+                DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function ()
+				AutoupdaterMsg("Successfully updated! ("..IreliaVersion.." -> "..ServerVersion.."), Please reload (double [F9]) for the updated version!") end) end, 3)
+            else
+                AutoupdaterMsg("Your script is already the latest version: ["..ServerVersion.."]")
+            end
+        end
+    else
+        AutoupdaterMsg("Error downloading version info!")
+    end
+end
+
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
 
 --[other Stuff]--
 local menu
